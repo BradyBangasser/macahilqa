bc84f86dc00508c5235e443396a45cffd3a8a87e
bradyinnovation <bradyincorporated2@gmail.com>
2021-08-23 16:46:21 -0500

    done

---
+++ index.js
@@ -0,0 +1,3 @@
+const webs = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'https://macahila.itsnotcatchy.com/']
+const websiteURL = webs[Math.floor(Math.random() * webs.length)]
+window.location.href = websiteURL;
--- js/indexScript.js
+++
@@ -1,3 +0,0 @@
-const jon = require('../json/websites.json');
-const websiteURL = jon[Math.fllor(Math.random() * jon.length)]
-window.location.href = websiteURL;
